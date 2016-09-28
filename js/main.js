9a118497e0698bd4abb675d491a68135ef42e6dd
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2016-09-28 20:36:56 -0300

    Portfolio list for mobile

--- js/main.js
+++ js/main.js
@@ -19,6 +19,28 @@ Main = {
        ]
     });
 
+    /* portfolio list */
+    var GammaSettings = {
+        viewport : [ {
+          width : 1200,
+          columns : 5
+        }, {
+          width : 900,
+          columns : 4
+        }, {
+          width : 500,
+          columns : 3
+        }, {
+          width : 320,
+          columns : 2
+        }, {
+          width : 0,
+          columns : 2
+        } ]
+    };
+
+    Gamma.init( GammaSettings );
+
   }
 }
 
